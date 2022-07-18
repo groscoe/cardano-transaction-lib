a5f386199d8e291f42421ebf7325f492f004a0be
GRoscoe <gustavo@gustavoroscoe.com>
2022-07-18 18:22:23 -0300

    Bump ODC

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
     # for the purescript project
     ogmios.url = "github:mlabs-haskell/ogmios/e406801eaeb32b28cd84357596ca1512bff27741";
-    ogmios-datum-cache.url = "github:mlabs-haskell/ogmios-datum-cache/98b1c4f2badc7ab1efe4be188ee9f9f5e4e54bb0";
+    ogmios-datum-cache.url = "github:mlabs-haskell/ogmios-datum-cache/bf76a74fa9e94d97310087dcda7b3aca259f96dd";
     # so named because we also need a different version of the repo below
     # in the server inputs and we use this one just for the `cardano-cli`
     # executables
